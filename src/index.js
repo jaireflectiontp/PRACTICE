3395c38cc153604deb5dd68bbc1f26af9dda4f1a
jaireflectiontp <jai.reflectiontp@gmail.com>
2022-11-28 23:13:06 -0800

    SetUp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
+//import App from './Components/A7-ValoForm/ValoForm-App';
 import App from './Components/A4-PortFolio/A4-App';
 import reportWebVitals from '../src/reportWebVitals';
 
